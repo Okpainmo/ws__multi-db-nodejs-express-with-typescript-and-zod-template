9bcec1d21f576f5bf853382c2218de8013fd640a
Okpainmo <okpainmoandrew@gmail.com>
2026-02-10 00:14:38 +0100

    refactor: config integration into prisma library

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client.js';
+import { dbConfig } from '../config/index.js';
 
-const connectionString = `${process.env.POSTGRES_DATABASE_URL}`;
+const connectionString = `${dbConfig.postgresql.url}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
