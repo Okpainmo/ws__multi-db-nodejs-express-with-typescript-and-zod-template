2d85b7d9bae621aca466bb76872ec50510c0b2ab
Okpainmo <okpainmoandrew@gmail.com>
2026-02-09 23:57:52 +0100

    chore: db migrations - init with user schema

---
+++ prisma/migrations/20260209155847_init_with_user_schema/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "accessToken" TEXT DEFAULT 'temp-token',
+    "refreshToken" TEXT DEFAULT 'temp-refresh-token',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
