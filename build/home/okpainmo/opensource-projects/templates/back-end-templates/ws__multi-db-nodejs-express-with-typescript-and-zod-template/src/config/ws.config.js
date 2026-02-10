560e6ce23845a6ea1384cf0ece5f2e1080f6921b
Okpainmo <okpainmoandrew@gmail.com>
2026-02-10 02:13:17 +0100

    build: project build directory with new structure

--- build/home/okpainmo/opensource-projects/templates/back-end-templates/ws__multi-db-nodejs-express-with-typescript-and-zod-template/src/config/ws.config.js
+++
@@ -1,7 +0,0 @@
-import validatedEnv from './base.js';
-export const wsConfig = {
-    path: validatedEnv.WS_PATH,
-    heartbeatInterval: validatedEnv.WS_HEARTBEAT_INTERVAL,
-    shutdownTimeout: validatedEnv.WS_SHUTDOWN_TIMEOUT
-};
-export default wsConfig;
